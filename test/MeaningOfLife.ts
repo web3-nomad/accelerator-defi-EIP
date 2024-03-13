e838dc16884b15d80fbd3aab5a0da992a0900912
Maksim <mks@unicsoft.com>
2024-03-13 19:03:36 +0100

    fix forking issue and refactor config

--- test/MeaningOfLife.ts
+++ test/MeaningOfLife.ts
@@ -1,15 +1,31 @@
-import { MeaningOfLife } from "../typechain-types";
 import { ethers, expect } from "./setup";
-  
+import { MeaningOfLife } from "../typechain-types";
+
+// constants
+// Tests
 describe("MeaningOfLife", function () {
-  let MeaningOfLifeContract: MeaningOfLife;
+  async function deployFixture() {
+    const [
+      owner,
+    ] = await ethers.getSigners();
+    let MeaningOfLifeContract: MeaningOfLife;
 
-  beforeEach(async () => {
-    const factory = await ethers.getContractFactory("MeaningOfLife");
-    MeaningOfLifeContract = await factory.deploy();
-  });
+    const MeaningOfLifeFac = await ethers.getContractFactory("MeaningOfLife");
+    MeaningOfLifeContract = await MeaningOfLifeFac.deploy();
+    await MeaningOfLifeContract.waitForDeployment();
 
-  it("Should be 42", async function () {
-    expect(await MeaningOfLifeContract.theMeaningOfLifeIs()).to.equal(42);
+    return {
+      MeaningOfLifeContract,
+      owner,
+    };
+  }
+
+  describe("test", function () {
+    it("Should pass", async function () {
+      const { MeaningOfLifeContract } = await deployFixture();
+      expect(
+        await MeaningOfLifeContract.theMeaningOfLifeIs()
+      ).to.eq(42);
+    });
   });
 });
